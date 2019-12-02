c419de317616869e9dc820564a43fabeac8e48ad
jonnysfarmer <john.farmer@latimerross.com>
2019-12-02 16:02:27 +0000

    navburger completed

--- coins/src/components/Home.js
+++ coins/src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => (
   <div className ="hero">
     <LazyHero imageSrc="https://newfinancial.org/wp-content/uploads/2019/01/iStock-869423492-Cover-image-full-2.jpg"
     isCentered = {true}
-    minHeight="95vh"
+    minHeight="100vh"
     transitionDuration={0}
     transitionTimingFunction="ease-in-out"
     color="#00003f"
--- coins/src/components/Navbar.js
+++ coins/src/components/Navbar.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { slide as Menu } from 'react-burger-menu'
 
 // https://negomi.github.io/react-burger-menu/
 
 
-const Navbar = () => (
-  // <Menu >
-  <div className="navbar1">
-    <Menu>
-    <ul>
-    <li><Link to="/" className="link">HOME</Link></li>
-    <li><Link to="/coins" className="link">COINS</Link></li>
-  </ul>
-  </Menu>
-  </div>
-  // </Menu>
-)
+const Navbar = () => {
+  const [menuOpen, setOpen] = useState(false)
+
+  return (
+    <Menu width={240}
+      disableAutoFocus
+      isOpen={menuOpen}
+      onStateChange={(state) => setOpen(state.menuOpen)}
+    >
+      {/* <div id='nav1'> */}
+      <Link to="/" className="link" onClick={() => setOpen(false)}>HOME</Link>
+      <Link to="/coins" className="link" onClick={() => setOpen(false)}>COINS</Link>
+      <Link to="/coins" className="link" onClick={() => setOpen(false)}>PORTFOLIO</Link>
+
+      <Link to="/coins" className="link" onClick={() => setOpen(false)}>LOGIN</Link>
+      <Link to="/coins" className="link" onClick={() => setOpen(false)}>RESGISTER</Link>
+      <Link to="/coins" className="link" onClick={() => setOpen(false)}>LOGOUT</Link>
+      {/* </div> */}
+    </Menu>
+  )
+
+}
 
 export default Navbar
