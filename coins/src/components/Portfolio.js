f44924f80ea8f1602cd34a960d7925e4bbb69537
jonnysfarmer <john.farmer@latimerross.com>
2020-01-02 19:01:24 +0000

    new portfolio modal

--- coins/src/components/Portfolio.js
+++ coins/src/components/Portfolio.js
@@ -2,18 +2,31 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Loader from './Loader'
 import Button from '@material-ui/core/Button'
+import Modal from '@material-ui/core/Modal'
+import Backdrop from '@material-ui/core/Backdrop'
+import Fade from '@material-ui/core/Fade'
+import TextField from '@material-ui/core/TextField'
+import Container from '@material-ui/core/Container'
 
 
 const Portfolio = (props) => {
 
   const [portfolio, setPortfolio] = useState([])
   const [errors, setErrors] = useState([])
+  const [modelState, setModelState] = useState(false)
 
   const portfolioHook = () => {
     const userID = props.match.params.userID
     axios.get(`http://localhost:8000/api/portfolio/${userID}`)
       .then(response => setPortfolio(response.data)) //this will only return list with a status of active
       .catch(err => setErrors(err))
+  }
+
+  const modelOpen = () => {
+    setModelState(true)
+  }
+  const modelClose = () => {
+    setModelState(false)
   }
 
   useEffect(portfolioHook, [])
@@ -23,16 +36,62 @@ const Portfolio = (props) => {
     <section className="hero is-fullheight background-white">
       <section className="section">
         <div className="container">
-        <div className="register-title">Portfolio</div>
-        <Button
+          <div className="register-title">Portfolio</div>
+          <Button
             type="submit"
             variant="contained"
             color="primary"
             className='button link'
             label="Portfolio"
+            onClick={modelOpen}
           >
             Create a Portfolio
           </Button>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className="modalportfolio"
+            open={modelState}
+            onClose={modelClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={modelState}>
+              <section className="section background-white">
+                <Container component="main" maxWidth="xs">
+                  <div className="register-title">New Portfolio</div>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    // className="background-white"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Portfolio Name"
+                    name="portfolioname"
+                    autoComplete="portfolioname"
+                    autoFocus
+                    color="white"
+                    // onChange={(e) => handleChange(e)}
+                    type="text"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className='button link'
+                    label="create"
+                  >
+                    Create
+                  </Button>
+                </Container>
+              </section>
+            </Fade>
+          </Modal>
 
 
 
@@ -40,7 +99,7 @@ const Portfolio = (props) => {
       </section>
     </section>
   )
-  
+
 
 
 }
